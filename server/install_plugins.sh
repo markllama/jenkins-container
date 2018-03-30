a4cdfc29324f3bd1821c25ee68c0f76e022cca1f
Mark Lamourine <mlamouri@redhat.com>
2018-03-30 03:21:04 +0000

    allow container to set user for jenkins, added tini and gosu

--- server/install_plugins.sh
+++ server/install_plugins.sh
@@ -1,40 +1,46 @@
 #!/bin/sh
 
 # From container environment
-JENKINS_HOME=${JENKINS_HOME:-"/jenkins"}
-JENKINS_WAR=${JENKINS_WAR:-"/usr/lib/jenkins/jenkins.war"}
-
-JAVA_ARGS=${JAVA_ARGS:-"-Dcom.sun.akuma.Daemon=daemonized 
+: "${JENKINS_ROOT:="/jenkins"}"
+: "${JENKINS_HOME:="${JENKINS_ROOT}/config"}"
+: "${JENKINS_WAR:="/usr/lib/jenkins/jenkins.war"}"
+: "${JAVA_ARGS:=-Dcom.sun.akuma.Daemon=daemonized 
 -Djava.awt.headless=true 
 -Djava.net.preferIPv4Stack=true
 -Djenkins.install.runSetupWizard=false \
--Djenkins.branch.WorkspaceLocatorImpl.PATH_MAX=0
--DJENKINS_HOME=${JENKINS_HOME}"}
+-Djenkins.branch.WorkspaceLocatorImpl.PATH_MAX=0}"
 
 # Only for plugin installation
-HTTP_PORT=${HTTP_PORT:-8080}
-HTTP_LISTEN_ADDRESS=${HTTP_LISTEN_ADDRESS:-127.0.0.1}
+: "${HTTP_PORT:="8080"}"
+: "${HTTP_LISTEN_ADDRESS:="127.0.0.1"}"
 HTTP_CONFIG="--httpPort=${HTTP_PORT} --httpListenAddress=${HTTP_LISTEN_ADDRESS}"
 
 # =============================================================================
 # Main
 # =============================================================================
+env | grep JENKINS
 
 # Start the service on a local address and port for CLI access
 cd ${JENKINS_HOME}
 
 /usr/bin/java ${JAVA_ARGS} -jar ${JENKINS_WAR} ${HTTP_CONFIG} &
+
+echo 'Waiting 10 seconds for Jenkins to start answering'
 sleep 10
-
 # Pull the CLI jar file from the running server
+echo '--- BEGIN RETRIEVE CLI BINARY ---'
 curl http://${HTTP_LISTEN_ADDRESS}:${HTTP_PORT}/jnlpJars/jenkins-cli.jar \
-     > ${JENKINS_HOME}/jenkins-cli.jar
-
+     > ${JENKINS_ROOT}/bin/jenkins-cli.jar
+echo '--- END RETRIEVE CLI BINARY ---'
+echo 'sleeping 5 seconds'
+sleep 5
+echo '--- BEGIN INSTALLING PLUGINS ---'
 # Load the plugins listed in the plugins.txt file
 for PLUGIN in $(cat /jenkins/plugins.txt) ; do
-    java -jar ${JENKINS_HOME}/jenkins-cli.jar -s http://127.0.0.1:8080 \
+    java -jar ${JENKINS_ROOT}/bin/jenkins-cli.jar -s http://127.0.0.1:8080 \
          install-plugin $PLUGIN
 done
+echo '--- END INSTALLING PLUGINS ---'
 
 # kill the local private service and move on
 pkill java
--- server/startup.sh
+++ server/startup.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
 # From container environment
-JENKINS_HOME=${JENKINS_HOME:-"/jenkins"}
-JENKINS_WAR=${JENKINS_WAR:-"/usr/lib/jenkins/jenkins.war"}
-HTTPS_PORT=${HTTPS_PORT:-8443}
-HTTPS_LISTEN_ADDRESS=${HTTPS_LISTEN_ADDRESS:-0.0.0.0}
-HTTPS_KEYSTORE=${HTTPS_KEYSTORE:-${JENKINS_HOME}/keystore.jks}
-HTTPS_KEYSTORE_PASSWORD=${HTTPS_KEYSTORE_PASSWORD:-${KEYSTORE_PASSWORD}}
+: "${JENKINS_ROOT:=/jenkins}"
+: "${JENKINS_HOME:=${JENKINS_ROOT}/config}"
+: "${JENKINS_WAR:=/usr/lib/jenkins/jenkins.war}"
+: "${JENKINS_USER:=jenkins}"
+: "${JENKINS_UID:=$(id -u ${JENKINS_USER})}"
+: "${JENKINS_GROUP:=jenkins}"
+: "${JENKINS_GID:=$(id -u ${JENKINS_GROUP})}"
+: "${HTTPS_PORT:=8443}"
+: "${HTTPS_LISTEN_ADDRESS:=0.0.0.0}"
+: "${HTTPS_KEYSTORE:=${JENKINS_ROOT}/keystore.jks}"
+: "${HTTPS_KEYSTORE_PASSWORD:=${KEYSTORE_PASSWORD}}"
 
-JAVA_ARGS=${JAVA_ARGS:-"-Dcom.sun.akuma.Daemon=daemonized
+: "${JAVA_ARGS:=Dcom.sun.akuma.Daemon=daemonized
 -Djava.awt.headless=true
 -Djava.net.preferIPv4Stack=true
 -Djenkins.install.runSetupWizard=false
 -Djenkins.branch.WorkspaceLocatorImpl.PATH_MAX=0
--DJENKINS_HOME=${JENKINS_HOME}"}
+-DJENKINS_HOME=${JENKINS_HOME}}"
 
 HTTPS_CONFIG="--httpPort=-1 
 --httpsPort=${HTTPS_PORT}
@@ -21,5 +26,30 @@ HTTPS_CONFIG="--httpPort=-1
 --httpsKeyStore=${HTTPS_KEYSTORE}
 --httpsKeyStorePassword=${HTTPS_KEYSTORE_PASSWORD}"
 
+if [ $JENKINS_UID -ne $(id -u ${JENKINS_USER}) ] ; then
+    usermod -u ${JENKINS_UID} ${JENKINS_USER}
+fi
+
+if [ $JENKINS_GID -ne $(id -g ${JENKINS_USER}) ] ; then
+    groupmod -g ${JENKINS_GID} ${JENKINS_GROUP}
+    usermod -g ${JENKINS_GID} ${JENKINS_USER}
+fi
+
+[ -d ${JENKINS_ROOT}/var/builds ] || mkdir -p ${JENKINS_ROOT}/var/builds
+[ -d ${JENKINS_ROOT}/var/workspaces ] || mkdir -p ${JENKINS_ROOT}/var/workspaces
+
+/usr/bin/sed \
+    -i \
+    -e "/buildsDir/s|>.*<|>$JENKINS_ROOT/var/builds/\${ITEM_FULL_NAME}<|" \
+    -e "/workspaceDir/s|>.*<|>$JENKINS_ROOT/var/workspaces/\${ITEM_FULL_NAME}<|" \
+    ${JENKINS_HOME}/config.xml
+
+chown -R ${JENKINS_USER}:${JENKINS_GROUP} ${JENKINS_HOME}
+chown ${JENKINS_USER}:${JENKINS_GROUP} ${JENKINS_ROOT}/backups
+chown ${JENKINS_USER}:${JENKINS_GROUP} ${JENKINS_ROOT}/secrets
+chown ${JENKINS_USER}:${JENKINS_GROUP} ${JENKINS_ROOT}/var/builds
+chown ${JENKINS_USER}:${JENKINS_GROUP} ${JENKINS_ROOT}/var/workspaces
+
 cd ${JENKINS_HOME}
-exec /usr/bin/java ${JAVA_ARGS} -jar ${JENKINS_WAR} ${HTTPS_CONFIG}
+exec gosu ${JENKINS_USER} \
+     /usr/bin/java ${JAVA_ARGS} -jar ${JENKINS_WAR} ${HTTPS_CONFIG}
